be06cad4b075c8560f4f533fcc77dcd8102d3a31
Madihamohammadishaque <77229999+Madihamohammadishaque@users.noreply.github.com>
2022-08-04 13:44:49 +0600

    Add files via upload

---
+++ res-20220804T073018Z-001/res/chunks/reviews.php
@@ -0,0 +1,78 @@
+<section class="freviews">
+
+		<div class="section white center">
+			<h3 class="header">What Our Customers Say</h3>
+			<div class="carousel myreviews" style="margin-bottom: 35px;">
+			    <a class="carousel-item" href="#one!">
+			    	<div class="row">
+					    <div class="col s12">
+					      <div class="card-panel teal" style="background: #ee6e73 !important;">
+					        <span class="white-text">"The food of this resturant is just like heaven for me! Its so delicious and tasty that I can't help going there every weekend!"<br>-<br> <strong>Mohammad Barkatullah</strong>
+					        </span>
+					      </div>
+					    </div>
+					  </div>
+			    </a>
+			    <a class="carousel-item" href="#two!">
+			    	<div class="row">
+					    <div class="col s12">
+					      <div class="card-panel teal" style="background: #ee6e73 !important;">
+					        <span class="white-text">"The food of this resturant is just like heaven for me! Its so delicious and tasty that I can't help going there every weekend!"<br>-<br> <strong>Raunak Afroz</strong>
+					        </span>
+					      </div>
+					    </div>
+					  </div>
+			    </a>
+			    <a class="carousel-item" href="#three!">
+			    	<div class="row">
+					    <div class="col s12">
+					      <div class="card-panel teal" style="background: #ee6e73 !important;">
+					        <span class="white-text">"The food of this resturant is just like heaven for me! Its so delicious and tasty that I can't help going there every weekend!"<br>-<br> <strong>Doha Mohammad</strong>
+					        </span>
+					      </div>
+					    </div>
+					  </div>
+			    </a>
+			    <a class="carousel-item" href="#four!">
+			    	<div class="row">
+					    <div class="col s12">
+					      <div class="card-panel teal" style="background: #ee6e73 !important;">
+					        <span class="white-text">"The food of this resturant is just like heaven for me! Its so delicious and tasty that I can't help going there every weekend!"<br>-<br> <strong>Sadia Alma</strong>
+					        </span>
+					      </div>
+					    </div>
+					  </div>
+			    </a>
+			    <a class="carousel-item" href="#five!">
+			    	<div class="row">
+					    <div class="col s12">
+					      <div class="card-panel teal" style="background: #ee6e73 !important;">
+					        <span class="white-text">"The food of this resturant is just like heaven for me! Its so delicious and tasty that I can't help going there every weekend!"<br>-<br> <strong>Nayem Ovi</strong>
+					        </span>
+					      </div>
+					    </div>
+					  </div>
+			    </a>
+			    <a class="carousel-item" href="#six!">
+			    	<div class="row">
+					    <div class="col s12">
+					      <div class="card-panel teal" style="background: #ee6e73 !important;">
+					        <span class="white-text">"The food of this resturant is just like heaven for me! Its so delicious and tasty that I can't help going there every weekend!"<br>-<br> <strong>Mahmud Hassan Rabby</strong>
+					        </span>
+					      </div>
+					    </div>
+					  </div>
+			    </a>
+			    <a class="carousel-item" href="#seven!">
+			    	<div class="row">
+					    <div class="col s12">
+					      <div class="card-panel teal" style="background: #ee6e73 !important;">
+					        <span class="white-text">"The food of this resturant is just like heaven for me! Its so delicious and tasty that I can't help going there every weekend!"<br>-<br> <strong>Afrin Jamil</strong>
+					        </span>
+					      </div>
+					    </div>
+					  </div>
+			    </a>
+			  </div>
+		</div>
+	</section>
